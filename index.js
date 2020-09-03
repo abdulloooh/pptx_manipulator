8de9c8700042602e823616f910b0999580059fbb
abdulloooh <abdullahakinwumi@gmail.com>
2020-09-03 16:37:03 +0100

    backbone for uploading and moving files

--- index.js
+++ index.js
@@ -1,8 +1,32 @@
+const formidable = require("formidable");
+const fs = require("fs");
 const express = require("express");
 const app = express();
 
-app.get("/file", (req, res) => {
-  res.send("hello");
+app.post("/fileupload", (req, res) => {
+  const form = new formidable.IncomingForm();
+  form.parse(req, function (err, fields, files) {
+    var oldpath = files.filetoupload.path;
+    var newpath = "./" + files.filetoupload.name;
+    fs.rename(oldpath, newpath, function (err) {
+      if (err) {
+        console.log(err.message);
+        res.sendStatus(500);
+      }
+      res.write("File uploaded and moved!");
+      res.end();
+    });
+  });
+});
+app.get("/", (req, res) => {
+  res.writeHead(200, { "Content-Type": "text/html" });
+  res.write(
+    '<form action="fileupload" method="post" enctype="multipart/form-data">'
+  );
+  res.write('<input type="file" name="filetoupload"><br>');
+  res.write('<input type="submit">');
+  res.write("</form>");
+  return res.end();
 });
 
 const port = process.env.PORT || 3000;
